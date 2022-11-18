c4ccc9d91f178367950c8c2122dca53efa39f394
ashish <20190802091@dypiu.ac.in>
2022-11-18 09:59:48 +0530

    created routers for post

--- api/index.js
+++ api/index.js
@@ -4,13 +4,15 @@ import dotenv from 'dotenv';
 
 import authRoute from './routes/auth.js';
 import userRoute from './routes/users.js';
+import postRoute from './routes/posts.js';
 
 const app = express();
 app.use(express.json());
 dotenv.config();
 
 app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 
 const connect = async () => {
     try {
